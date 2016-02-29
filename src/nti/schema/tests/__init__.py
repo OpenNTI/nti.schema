f8bba5f6c2d5b66ebff84113685a2f90616da245
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-02-28 21:41:28 -0600

    clean tests

--- src/nti/schema/tests/__init__.py
+++ src/nti/schema/tests/__init__.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-
-
-.. $Id$
-"""
 
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
 try:
 	# canonical location, not necessarily public yet
@@ -18,18 +16,24 @@ try:
 except ImportError:
 	# knock off versions that work but lack flexibility
 	import gc
+	import os
+
 	from zope import component
-	from zope.configuration import xmlconfig, config
-	from zope.component.hooks import setHooks
-	from zope.dottedname import resolve as dottedname
+
 	from zope.component import eventtesting
+	from zope.component.hooks import setHooks
+
+	from zope.configuration import config
+	from zope.configuration import xmlconfig
+
+	from zope.dottedname import resolve as dottedname
 
 	import zope.testing.cleanup
 
 	import transaction
-	import os
 
 	class SharedCleanupLayer(object):
+
 		@classmethod
 		def setUp(cls):
 			# You MUST implement this, otherwise zope.testrunner
@@ -51,29 +55,32 @@ except ImportError:
 			pass
 
 	class ZopeComponentLayer(SharedCleanupLayer):
-		@classmethod
-		def setUp( cls ):
-			setHooks() # zope.component.hooks registers a zope.testing.cleanup to reset these
-
 
 		@classmethod
-		def tearDown( cls ):
+		def setUp(cls):
+			setHooks()  # zope.component.hooks registers a zope.testing.cleanup to reset these
+
+		@classmethod
+		def tearDown(cls):
 			# always safe to clear events
-			eventtesting.clearEvents() # redundant with zope.testing.cleanup
+			eventtesting.clearEvents()  # redundant with zope.testing.cleanup
 			# resetHooks()  we never actually want to do this, it's not needed and can mess up other fixtures
 
 		@classmethod
 		def testSetUp(cls):
-			setHooks() # ensure these are still here; cheap and easy
+			setHooks()  # ensure these are still here; cheap and easy
 
 		@classmethod
-		def testTearDown( cls ):
+		def testTearDown(cls):
 			# Some tear down needs to happen always
 			eventtesting.clearEvents()
-			transaction.abort() # see comments above
+			transaction.abort()  # see comments above
 
-
-	def _configure(self=None, set_up_packages=(), features=('devmode','testmode'), context=None, package=None):
+	def _configure(self=None,
+				   set_up_packages=(),
+				   features=('devmode', 'testmode'),
+				   context=None,
+				   package=None):
 
 		if features is not None:
 			features = set(features)
@@ -83,56 +90,56 @@ except ImportError:
 		# This is normally created by a slug, but tests may not always
 		# load the slug
 		if os.getenv('DATASERVER_DIR_IS_BUILDOUT'):
-			features.add( 'in-buildout' )
-
+			features.add('in-buildout')
 
 		# zope.component.globalregistry conveniently adds
 		# a zope.testing.cleanup.CleanUp to reset the globalSiteManager
 		if context is None and (features or package):
 			context = config.ConfigurationMachine()
 			context.package = package
-			xmlconfig.registerCommonDirectives( context )
+			xmlconfig.registerCommonDirectives(context)
 
 		for feature in features:
-			context.provideFeature( feature )
+			context.provideFeature(feature)
 
 		if set_up_packages:
-			logger.debug( "Configuring %s with features %s", set_up_packages, features )
+
+			logger.debug("Configuring %s with features %s", set_up_packages, features)
 
 			for i in set_up_packages:
 				__traceback_info__ = (i, self)
-				if isinstance( i, tuple ):
+				if isinstance(i, tuple):
 					filename = i[0]
 					package = i[1]
 				else:
 					filename = 'configure.zcml'
 					package = i
 
-				if isinstance( package, basestring ):
-					package = dottedname.resolve( package )
+				if isinstance(package, basestring):
+					package = dottedname.resolve(package)
 
 				try:
-					context = xmlconfig.file( filename, package=package, context=context )
+					context = xmlconfig.file(filename, package=package, context=context)
 				except IOError as e:
 					# Did we pass in a test module (__name__) and there is no
 					# configuration in that package? In that case, we want to
 					# configure the parent package for sure
-					module_path = getattr( package, '__file__', None )
+					module_path = getattr(package, '__file__', None)
 					if (module_path
 						and 'tests' in module_path
 						and os.path.join(os.path.dirname(module_path), filename) == e.filename):
 						parent_package_name = '.'.join(package.__name__.split('.')[:-2])
-						package = dottedname.resolve( parent_package_name )
-						context = xmlconfig.file( filename, package=package, context=context )
+						package = dottedname.resolve(parent_package_name)
+						context = xmlconfig.file(filename, package=package, context=context)
 					else:
 						raise
 
 		return context
 
-
 	class ConfiguringLayerMixin(object):
+
 		set_up_packages = ()
-		features = ('devmode','testmode')
+		features = ('devmode', 'testmode')
 		configuration_context = None
 
 		@classmethod
@@ -148,7 +155,7 @@ except ImportError:
 			pass
 
 		@classmethod
-		def testSetUp( cls ):
+		def testSetUp(cls):
 			pass
 
 		@classmethod
@@ -162,11 +169,11 @@ except ImportError:
 			cls.configuration_context = cls.configure_packages(set_up_packages=cls.set_up_packages,
 															   features=cls.features,
 															   context=cls.configuration_context)
-			component.provideHandler( eventtesting.events.append, (None,) )
+			component.provideHandler(eventtesting.events.append, (None,))
 			gc.collect()
 
 		@classmethod
-		def configure_packages(cls, set_up_packages=(), features=(), context=None ):
+		def configure_packages(cls, set_up_packages=(), features=(), context=None):
 			cls.configuration_context = _configure(self=cls,
 												   set_up_packages=set_up_packages,
 												   features=features,
@@ -178,5 +185,6 @@ except ImportError:
 			# This is a duplicate of zope.component.globalregistry
 			gc.collect()
 			component.getGlobalSiteManager().__init__('base')
+
 			gc.collect()
 			cls.configuration_context = None
--- src/nti/schema/tests/test_schema.py
+++ src/nti/schema/tests/test_schema.py
@@ -9,80 +9,89 @@ logger = __import__('logging').getLogger(__name__)
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-from hamcrest import assert_that
 from hamcrest import is_
-from hamcrest import has_property
-from hamcrest import has_length
-from hamcrest import contains
-from hamcrest import has_key
-from hamcrest import calling
-from hamcrest import raises
 from hamcrest import none
 from hamcrest import is_not
-from hamcrest import not_none
-from hamcrest import has_entry
+from hamcrest import raises
+from hamcrest import calling
+from hamcrest import has_key
 from hamcrest import has_item
-import unittest
+from hamcrest import not_none
+from hamcrest import contains
+from hamcrest import has_entry
+from hamcrest import has_length
+from hamcrest import assert_that
+from hamcrest import has_property
 does_not = is_not
 
-from nti.schema.testing import verifiably_provides, validated_by, not_validated_by
+import unittest
 
-
-from ..field import HTTPURL, Variant, ObjectLen, Object
-#from ..schema import DataURI
-from ..interfaces import IVariant
-from ..field import Number
-from ..field import DictFromObject
-from ..field import ListOrTuple
-from ..field import ValidRegularExpression
-from ..field import ValidTextLine as TextLine
-from ..interfaces import IBeforeSequenceAssignedEvent
-from ..interfaces import IBeforeDictAssignedEvent
-from ..fieldproperty import createFieldProperties
-from ..fieldproperty import createDirectFieldProperties
-
-from ..jsonschema import JsonSchemafier
-
-from dolmen.builtins import IUnicode
 from zope import interface
+
 from zope.interface.common import interfaces as cmn_interfaces
 
-from zope.schema import interfaces as sch_interfaces
 from zope.schema import Dict
+
+from zope.schema import interfaces as sch_interfaces
+
 from zope.schema.interfaces import InvalidURI
+
+from dolmen.builtins import IUnicode
+
+from nti.schema.field import Number
+from nti.schema.field import Object
+from nti.schema.field import HTTPURL
+from nti.schema.field import Variant
+from nti.schema.field import ObjectLen
+from nti.schema.field import ListOrTuple
+from nti.schema.field import DictFromObject
+from nti.schema.field import ValidRegularExpression
+from nti.schema.field import ValidTextLine as TextLine
+
+from nti.schema.fieldproperty import createFieldProperties
+from nti.schema.fieldproperty import createDirectFieldProperties
+
+from nti.schema.interfaces import IVariant
+from nti.schema.interfaces import IBeforeDictAssignedEvent
+from nti.schema.interfaces import IBeforeSequenceAssignedEvent
+
+from nti.schema.jsonschema import JsonSchemafier
+
+from nti.schema.testing import validated_by
+from nti.schema.testing import not_validated_by
+from nti.schema.testing import verifiably_provides
 
 class TestMisc(unittest.TestCase):
 
 	def test_fixup_name(self):
 		from zope.schema.fieldproperty import FieldPropertyStoredThroughField
 
-		field = Object( IUnicode )
+		field = Object(IUnicode)
 		field.__name__ = 'field'
 
 		field_property = FieldPropertyStoredThroughField(field)
 		field = field_property.field
 
-		assert_that( field, has_property('__name__', '__st_field_st') )
-
-		assert_that( field, has_property('__fixup_name__', 'field'))
+		assert_that(field, has_property('__name__', '__st_field_st'))
+		assert_that(field, has_property('__fixup_name__', 'field'))
 
 	def test_objectlen(self):
 		# If we have the inheritance messed up, we will have problems
 		# creating, or we will have problems validating one part or the other.
 
-		olen = ObjectLen( IUnicode, max_length=5 ) # default val for min_length
+		olen = ObjectLen(IUnicode, max_length=5)  # default val for min_length
 
-		olen.validate( 'a' )
-		olen.validate( '' )
+		olen.validate('a')
+		olen.validate('')
 
-		assert_that( calling(olen.validate).with_args(object()),
+		assert_that(calling(olen.validate).with_args(object()),
 					 raises(sch_interfaces.SchemaNotProvided))
 
-		assert_that( calling(olen.validate).with_args('abcdef'),
-					 raises(sch_interfaces.TooLong) )
-
+		assert_that(calling(olen.validate).with_args('abcdef'),
+					 raises(sch_interfaces.TooLong))
 
 	def test_create_direct_field_properties(self):
+
 		class IA(interface.Interface):
 			a = TextLine(title="a")
 
@@ -95,34 +104,33 @@ class TestMisc(unittest.TestCase):
 		class B(object):
 			createDirectFieldProperties(IB)
 
-		assert_that( A.__dict__, has_key( 'a' ) )
-		assert_that( B.__dict__, has_key( 'b' ) )
-		assert_that( B.__dict__, does_not( has_key( 'a' ) ) )
+		assert_that(A.__dict__, has_key('a'))
+		assert_that(B.__dict__, has_key('b'))
+		assert_that(B.__dict__, does_not(has_key('a')))
+
 		# And nothing extra crept in, just the four standard things
 		# __dict__, __doct__, __module__, __weakref__, and b
-		assert_that( B.__dict__, has_length( 5 ) )
-
+		assert_that(B.__dict__, has_length(5))
 
 	def test_http_url(self):
 
 		http = HTTPURL(__name__='foo')
 
-		assert_that( http.fromUnicode( 'www.google.com' ),
-					 is_( 'http://www.google.com' ) )
+		assert_that(http.fromUnicode('www.google.com'),
+					 is_('http://www.google.com'))
 
-		assert_that( http.fromUnicode( 'https://www.yahoo.com' ),
-					 is_( 'https://www.yahoo.com' ) )
+		assert_that(http.fromUnicode('https://www.yahoo.com'),
+					 is_('https://www.yahoo.com'))
 
 		try:
-			http.fromUnicode( 'mailto:jason@nextthought.com' )
+			http.fromUnicode('mailto:jason@nextthought.com')
 			self.fail("Must raise")
 		except InvalidURI as ex:
 			exception = ex
 
-
-		assert_that( exception, has_property( 'field', http ) )
-		assert_that( exception, has_property( 'value', 'mailto:jason@nextthought.com' ) )
-		assert_that( exception, has_property( 'message', 'The specified URL is not valid.' ) )
+		assert_that(exception, has_property('field', http))
+		assert_that(exception, has_property('value', 'mailto:jason@nextthought.com'))
+		assert_that(exception, has_property('message', 'The specified URL is not valid.'))
 
 	# def test_data_uri():
 	# 	from .test_dataurl import GIF_DATAURL
@@ -145,71 +153,72 @@ class TestMisc(unittest.TestCase):
 
 	def test_variant(self):
 
-		syntax_or_lookup = Variant( (Object(cmn_interfaces.ISyntaxError),Object(cmn_interfaces.ILookupError), Object(IUnicode)) )
+		syntax_or_lookup = Variant((Object(cmn_interfaces.ISyntaxError),
+									Object(cmn_interfaces.ILookupError),
+									Object(IUnicode)))
 
-		assert_that( syntax_or_lookup, verifiably_provides( IVariant ) )
+		assert_that(syntax_or_lookup, verifiably_provides(IVariant))
 
 		# validates
-		assert_that( SyntaxError(), validated_by( syntax_or_lookup ) )
-		assert_that( LookupError(), validated_by( syntax_or_lookup ) )
+		assert_that(SyntaxError(), validated_by(syntax_or_lookup))
+		assert_that(LookupError(), validated_by(syntax_or_lookup))
 
 		# doesn't validate
-		assert_that( b'foo', not_validated_by( syntax_or_lookup ) )
+		assert_that(b'foo', not_validated_by(syntax_or_lookup))
 
-		assert_that( syntax_or_lookup.fromObject( 'foo' ), is_( 'foo' ) )
+		assert_that(syntax_or_lookup.fromObject('foo'), is_('foo'))
 
-		assert_that( calling( syntax_or_lookup.fromObject).with_args(object()),
-					 raises(TypeError) )
-
+		assert_that(calling(syntax_or_lookup.fromObject).with_args(object()),
+					 raises(TypeError))
 
 		# cover
 		syntax_or_lookup.getDoc()
 
 	def test_complex_variant(self):
 
-		dict_field = Dict( key_type=TextLine(), value_type=TextLine() )
+		dict_field = Dict(key_type=TextLine(), value_type=TextLine())
 		string_field = Object(IUnicode)
-		list_of_numbers_field = ListOrTuple( value_type=Number() )
+		list_of_numbers_field = ListOrTuple(value_type=Number())
 
-		variant = Variant( (dict_field, string_field, list_of_numbers_field) )
-		variant.getDoc() # cover
+		variant = Variant((dict_field, string_field, list_of_numbers_field))
+		variant.getDoc()  # cover
 		# It takes all these things
 		for d in { 'k': 'v'}, 'foo', [1, 2, 3]:
-			assert_that( d, validated_by( variant ) )
+			assert_that(d, validated_by(variant))
 
 		# It rejects these
 		for d in {'k': 1}, b'foo', [1, 2, 'b']:
-			assert_that( d, not_validated_by( variant ) )
-
+			assert_that(d, not_validated_by(variant))
 
 		# A name set now is reflected down the line
 		variant.__name__ = 'baz'
 		for f in variant.fields:
-			assert_that( f, has_property( '__name__', 'baz' ) )
+			assert_that(f, has_property('__name__', 'baz'))
 
 		# and in clones
-		clone = variant.bind( object() )
+		clone = variant.bind(object())
 		for f in clone.fields:
-			assert_that( f, has_property( '__name__', 'baz' ) )
+			assert_that(f, has_property('__name__', 'baz'))
 
 		# which doesn't change the original
 		clone.__name__ = 'biz'
 		for f in clone.fields:
-			assert_that( f, has_property( '__name__', 'biz' ) )
-		for f in variant.fields:
-			assert_that( f, has_property( '__name__', 'baz' ) )
+			assert_that(f, has_property('__name__', 'biz'))
 
+		for f in variant.fields:
+			assert_that(f, has_property('__name__', 'baz'))
 
 		# new objects work too
-		new = Variant( variant.fields, __name__='boo' )
+		new = Variant(variant.fields, __name__='boo')
 		for f in new.fields:
-			assert_that( f, has_property( '__name__', 'boo' ) )
+			assert_that(f, has_property('__name__', 'boo'))
 
 from zope.component import eventtesting
+
 from zope.testing import cleanup
 
-from . import ZopeComponentLayer
-from . import ConfiguringLayerMixin
+from nti.schema.tests import ZopeComponentLayer
+from nti.schema.tests import ConfiguringLayerMixin
 
 class SchemaLayer(ZopeComponentLayer,
 				  ConfiguringLayerMixin):
@@ -232,85 +241,89 @@ class SchemaLayer(ZopeComponentLayer,
 	testTearDown = testSetUp
 
 class TestConfigured(unittest.TestCase):
+
 	layer = SchemaLayer
 
-
 	def test_nested_variants(self):
-		# Use case: Chat messages are either a Dict, or a Note-like body, which itself is a list of variants
+		# Use case: Chat messages are either a Dict, or a N
+		# ote-like body, which itself is a list of variants
 
-		dict_field = Dict( key_type=TextLine(), value_type=TextLine() )
+		dict_field = Dict(key_type=TextLine(), value_type=TextLine())
 
 		string_field = Object(IUnicode)
 		number_field = Number()
-		list_of_strings_or_numbers = ListOrTuple( value_type=Variant( (string_field, number_field) ) )
+		list_of_strings_or_numbers = ListOrTuple(value_type=Variant((string_field, number_field)))
 
-		assert_that( [1, '2'], validated_by( list_of_strings_or_numbers ) )
-		assert_that( {'k': 'v'}, validated_by( dict_field ) )
+		assert_that([1, '2'], validated_by(list_of_strings_or_numbers))
+		assert_that({'k': 'v'}, validated_by(dict_field))
 
-		dict_or_list = Variant( ( dict_field, list_of_strings_or_numbers ) )
+		dict_or_list = Variant((dict_field, list_of_strings_or_numbers))
 
-		assert_that( [1, '2'], validated_by( dict_or_list ) )
-		assert_that( {'k': 'v'}, validated_by( dict_or_list ) )
-
+		assert_that([1, '2'], validated_by(dict_or_list))
+		assert_that({'k': 'v'}, validated_by(dict_or_list))
 
 		class X(object):
 			pass
-		x = X()
-		dict_or_list.set( x, [1, '2'] )
 
-		events = eventtesting.getEvents( IBeforeSequenceAssignedEvent )
-		assert_that( events, has_length( 1 ) )
-		assert_that( events, contains( has_property( 'object', [1, '2'] ) ) )
+		x = X()
+		dict_or_list.set(x, [1, '2'])
+
+		events = eventtesting.getEvents(IBeforeSequenceAssignedEvent)
+		assert_that(events, has_length(1))
+		assert_that(events, contains(has_property('object', [1, '2'])))
 
 		eventtesting.clearEvents()
 
-		dict_or_list.set( x, {'k': 'v'} )
-		events = eventtesting.getEvents( IBeforeDictAssignedEvent )
-		assert_that( events, has_length( 1 ) )
-		assert_that( events, contains( has_property( 'object', {'k': 'v'} ) ) )
+		dict_or_list.set(x, {'k': 'v'})
+		events = eventtesting.getEvents(IBeforeDictAssignedEvent)
+		assert_that(events, has_length(1))
+		assert_that(events, contains(has_property('object', {'k': 'v'})))
 
 	def test_dict(self):
 
-		dict_field = DictFromObject( key_type=TextLine(), value_type=Number() )
+		dict_field = DictFromObject(key_type=TextLine(), value_type=Number())
 		dict_field.__name__ = 'dict'
 
 		class X(object):
 			pass
+
 		x = X()
-		dict_field.set( x, dict_field.fromObject({'k': '1'}) )
+		dict_field.set(x, dict_field.fromObject({'k': '1'}))
 
-		assert_that( x, has_property('dict', {'k': 1.0} ))
+		assert_that(x, has_property('dict', {'k': 1.0}))
 
-		events = eventtesting.getEvents( IBeforeDictAssignedEvent )
-		assert_that( events, has_length( 1 ) )
-		assert_that( events, contains( has_property( 'object', {'k': 1.0} ) ) )
-
-
+		events = eventtesting.getEvents(IBeforeDictAssignedEvent)
+		assert_that(events, has_length(1))
+		assert_that(events, contains(has_property('object', {'k': 1.0})))
 
 	def test_country_vocabulary(self):
 		from zope.schema import Choice
+
 		class IA(interface.Interface):
 			choice = Choice(title="Choice",
 							vocabulary="Countries")
 
 		o = object()
 
-		choice = IA['choice'].bind( o )
-		assert_that( choice.vocabulary, is_( not_none() ) )
-		term = choice.vocabulary.getTermByToken( 'us' )
-		assert_that( term, has_property( 'value', "United States" ) )
+		choice = IA['choice'].bind(o)
+		assert_that(choice.vocabulary, is_(not_none()))
+		term = choice.vocabulary.getTermByToken('us')
+		assert_that(term, has_property('value', "United States"))
 		ext = term.toExternalObject()
-		assert_that( ext, has_entry( 'flag', u'/++resource++country-flags/us.gif' ) )
-		assert_that( ext, has_entry( 'title', 'United States'  ) )
+		assert_that(ext, has_entry('flag', u'/++resource++country-flags/us.gif'))
+		assert_that(ext, has_entry('title', 'United States'))
 
-		schema = JsonSchemafier( IA ).make_schema()
-		assert_that( schema, has_entry( 'choice', has_entry( 'choices', has_item( ext ) ) ) )
+		schema = JsonSchemafier(IA).make_schema()
+		assert_that(schema, has_entry('choice', has_entry('choices', has_item(ext))))
 
 try:
-	from nti.testing.matchers import aq_inContextOf
-	from ..fieldproperty import AcquisitionFieldProperty
 	from Acquisition import Implicit
+
 	from ExtensionClass import Base
+
+	from nti.schema.fieldproperty import AcquisitionFieldProperty
+
+	from nti.testing.matchers import aq_inContextOf
 except ImportError:
 	pass
 else:
@@ -321,6 +334,7 @@ else:
 
 			class IBaz(interface.Interface):
 				pass
+
 			class IFoo(interface.Interface):
 				ob = Object(IBaz)
 
@@ -328,26 +342,25 @@ else:
 			class Baz(object):
 				pass
 
-			class BazAQ(Implicit,Baz):
+			class BazAQ(Implicit, Baz):
 				pass
 
 			@interface.implementer(IFoo)
 			class Foo(Base):
 				ob = AcquisitionFieldProperty(IFoo['ob'])
 
-			assert_that( Foo, has_property( 'ob', is_( AcquisitionFieldProperty ) ) )
+			assert_that(Foo, has_property('ob', is_(AcquisitionFieldProperty)))
 
 			foo = Foo()
-			assert_that( foo, has_property( 'ob', none() ) )
+			assert_that(foo, has_property('ob', none()))
 
 			foo.ob = Baz()
-			assert_that( foo, has_property( 'ob', is_not( aq_inContextOf( foo ) ) ) )
+			assert_that(foo, has_property('ob', is_not(aq_inContextOf(foo))))
 
 			foo.ob = BazAQ()
-			assert_that( foo, has_property( 'ob', aq_inContextOf( foo ) ) )
+			assert_that(foo, has_property('ob', aq_inContextOf(foo)))
 
-
-from ..schema import _superhash as superhash
+from nti.schema.schema import _superhash as superhash
 
 class TestSuperHash(unittest.TestCase):
 
@@ -365,10 +378,10 @@ class TestSuperHash(unittest.TestCase):
 
 		assert_that(superhash(d),
 					is_(
-						((1,1),
+						((1, 1),
 						 (2, (1, 2, 3)),
 						 (3, ((4, (4, 5, 6)),)))
 					))
 
 		assert_that(hash(superhash(d)),
-					is_(-6213620179105025536) )
+					is_(-6213620179105025536))
